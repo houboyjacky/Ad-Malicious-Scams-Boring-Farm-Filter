e68aab58bb635d290ff19da28fc069c57ba2a15c
Jacky Hou <houboyjacky@gmail.com>
2023-05-26 09:54:05 +0800

    20230526 Update 3

--- Server/LineBot/Tools.py
+++ Server/LineBot/Tools.py
@@ -218,3 +218,7 @@ def read_file(file_path):
     with open(file_path, 'r') as file:
         lines = file.readlines()
     return ''.join(lines)
+
+def write_empty_file(file_path):
+    open(file_path, 'w').close()
+    return
