cab471da03247af7bd54dacf28d5adbc8ac59dac
Jacky Hou <houboyjacky@gmail.com>
2023-07-27 18:01:41 +0800

    20230727 Update 9

--- LineBot/Query_Line_Invite.py
+++ LineBot/Query_Line_Invite.py
@@ -82,7 +82,7 @@ def analyze_line_invite_url(user_text: str) -> Optional[dict]:
                 if f"{domain}.{tdl}" != "line.me":
                     logger.info("已經指向其他網域")
                     return None
-                match = re.search(r"accountId\%3D([a-z0-9]+)", redirected_url1)
+                match = re.search(r"accountId\%3D([a-z0-9_]+)", redirected_url1)
                 invite_code = match.group(1)
                 invite_code = f"@{invite_code}"
                 logger.info(f"invite_code={invite_code}")
