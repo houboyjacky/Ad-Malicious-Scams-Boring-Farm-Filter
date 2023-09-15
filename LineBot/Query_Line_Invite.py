f08024338ae99910bd4fec6a157ff6e27d6df9bc
Jacky Hou <houboyjacky@gmail.com>
2023-09-15 11:10:33 +0800

    20230915 Update 2

--- LineBot/Query_Line_Invite.py
+++ LineBot/Query_Line_Invite.py
@@ -124,6 +124,9 @@ def analyze_line_invite_url(user_text: str) -> Optional[dict]:
             logger.error('無法解析類別')
             return None
 
+        if "?fbclid" in user_text:
+            user_text = re.sub(r"\?fbclid=.+$","", user_text)
+
         struct = {"類別": category, "帳號": invite_code, "來源": orgin_text,
                   "回報次數": 0, "失效": 0, "檢查者": "", "加入日期": datetime}
 
