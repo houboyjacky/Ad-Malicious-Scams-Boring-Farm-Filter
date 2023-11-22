0b124a9730cbc3ab2a35b2e456318a06ef1e5633
Jacky Hou <houboyjacky@gmail.com>
2023-11-22 11:55:33 +0800

    20231122 Update 2

--- LineBot/Query_Instagram.py
+++ LineBot/Query_Instagram.py
@@ -62,8 +62,8 @@ def analyze_IG_url(user_text: str) -> Optional[dict]:
     logger.info(f"Code: {Code}")
     datetime = date.today().strftime("%Y-%m-%d")
 
-    struct = {"帳號": Username, "來源": user_text,
-              "回報次數": 0, "失效": 0, "檢查者": "", "加入日期": datetime}
+    struct = {"帳號": Username, "來源": user_text, "回報次數": 0, "失效": 0,
+              "檢查者": "", "加入日期": datetime, "查詢次數": 1, "詐騙與否": True}
 
     return struct
 
