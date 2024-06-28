64f301fab7f445678553bad6e9faa55feb4a5232
Jacky Hou <houboyjacky@gmail.com>
2024-06-28 11:40:52 +0800

    20240628 Update

--- Script/GetHash.py
+++ Script/GetHash.py
@@ -2,11 +2,24 @@ import hashlib
 import os
 import json
 
+
+def convert_to_lf_and_save(file_path):
+    with open(file_path, 'rb') as file:
+        content = file.read()
+
+    new_content = content.replace(b'\r\n', b'\n')
+
+    if new_content != content:
+        with open(file_path, 'wb') as file:
+            file.write(new_content)
+
+
 def calculate_hash(file_path):
     with open(file_path, 'rb') as file:
         content = file.read()
         hash_value = hashlib.md5(content).hexdigest()
         return hash_value
+
 
 def get_txt_files(directory):
     txt_files = []
@@ -17,17 +30,21 @@ def get_txt_files(directory):
     txt_files.sort()  # 對列表進行排序
     return txt_files
 
+
 def generate_hash_json(directory):
     txt_files = get_txt_files(directory)
     hash_dict = {}
 
     for file_path in txt_files:
+        convert_to_lf_and_save(file_path)
+
         file_name = os.path.basename(file_path)
         hash_value = calculate_hash(file_path)
         hash_dict[file_name] = hash_value
 
     with open('hashes.json', 'w', encoding='utf-8', newline='') as json_file:
         json.dump(hash_dict, json_file, indent=4)
+
 
 # 執行範例
 directory_path = '..'
